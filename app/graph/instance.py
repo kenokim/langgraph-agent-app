86f5be0cfb691f5ce6abc2056d228645a51bba15
김건호 <keno1413@gmail.com>
2025-05-20 22:23:25 +0900

    Delete MCP

--- app/graph/instance.py
+++ app/graph/instance.py
@@ -18,15 +18,16 @@ llm = ChatGoogleGenerativeAI(
 )
 
 # Grafana 에이전트
-grafana_agent = make_grafana_agent(llm)
-grafana_renderer_agent = make_grafana_renderer_agent(llm)
+#grafana_agent = make_grafana_agent(llm)
+#grafana_renderer_agent = make_grafana_renderer_agent(llm)
 
 # Server Info 에이전트
 server_info_agent = make_server_info_agent(llm)
 
 # Supervisor 그래프
 supervisor_graph = create_supervisor(
-    agents=[grafana_agent, grafana_renderer_agent, server_info_agent],
+    #agents=[grafana_agent, grafana_renderer_agent, server_info_agent],
+    agents=[server_info_agent],    
     model=llm,
     prompt=(
         "너는 명령을 듣고 답하는 에이전트야. 그라파나 대시보드를 보여달라고 요청받으면, grafana_renderer_agent에게 명령을 전달해줘. 그라파나 대시보드 관련 명령인데, 보여달라는 요청 외의 것은 grafana_agent에게 명령을 전달해줘."
